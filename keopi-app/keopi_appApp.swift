4927cca540f7ca6777512c3c29dad0a472602af1
Luka <vujnovacluka89@gmail.com>
2022-01-05 17:16:03 +0100

    Initial Commit

---
+++ keopi-app/keopi_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  keopi_appApp.swift
+//  keopi-app
+//
+//  Created by Luka Vujnovac on 05.01.2022..
+//
+
+import SwiftUI
+
+@main
+struct keopi_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
